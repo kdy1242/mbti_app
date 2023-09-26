258ac0132b7002a387b504e1e7528c3783adc94f
gong <ddddy0114@gmail.com>
2023-09-26 11:44:46 +0900

    내가해냄

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,19 +1,111 @@
 
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:mbti_app/question.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  PageController pageController = PageController();
+  List<List<String>> answer = List.generate(7, (_) => List.generate(7, (_) => '')); // a, b 선택값이 담겨있는 2차원배열
+  String mbti = '';
+
+  void updateChoice(int row, int column, String value) {
+    setState(() {
+      answer[row][column] = value;
+      log('$answer', name: 'answer');
+    });
+  }
+
+  void getMbti() {
+    int eScore = 0, iScore = 0, sScore = 0, nScore = 0, tScore = 0, fScore = 0, jScore = 0, pScore = 0;
+    String ei, sn, tf, jp;
+    for (int i=0; i<answer.length; i++) {
+      if (answer[i][0] == 'a') eScore++; else iScore++;
+      if (answer[i][1] == 'a' || answer[i][2] == 'a') sScore++; else nScore++;
+      if (answer[i][3] == 'a' || answer[i][4] == 'a') tScore++; else fScore++;
+      if (answer[i][5] == 'a' || answer[i][6] == 'a') jScore++; else pScore++;
+    }
+    log('e: $eScore, i: $iScore, s: $sScore, n: $nScore, t: $tScore, f: $fScore, j: $jScore, p:$pScore');
+    if (eScore > iScore) ei = 'e'; else ei = 'i';
+    if (sScore > nScore) sn = 's'; else sn = 'n';
+    if (tScore > fScore) tf = 't'; else tf = 'f';
+    if (jScore > pScore) jp = 'j'; else jp = 'p';
+    mbti = '${ei+sn+tf+jp}';
+    setState(() {});
+    log('$mbti', name: 'mbti');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('어케하지'),
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-        foregroundColor: Colors.black,
-        elevation: 0,
-      ),
+    return PageView.builder(
+      controller: pageController,
+      itemCount: questions.length + 1,
+      itemBuilder: (context, index) {
+        int row = index ~/ 7;
+        int column = index % 7;
+        return index < questions.length ? Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('${index + 1}. ${questions[index]['question']}'),
+            Text('A: ${questions[index]['options'][0]}'),
+            Text('A: ${questions[index]['options'][1]}'),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    updateChoice(row, column, 'a');
+                    pageController.nextPage(duration: Duration(seconds: 1), curve: Curves.ease);
+                  },
+                  child: Text('A'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    updateChoice(row, column, 'b');
+                    pageController.nextPage(duration: Duration(seconds: 1), curve: Curves.ease);
+                  },
+                  child: Text('B'),
+                ),
+              ],
+            )
+          ],
+        ) : Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                log('$answer', name: '결과보기');
+                List notSolveQuestion = [];
+                for(int i=0; i<answer.length; i++) {
+                  for (int j=0; j<answer[i].length; j++) {
+                    if(answer[i][j] == '') {
+                      int questionNumber = i * answer[i].length + j + 1;
+                      notSolveQuestion.add('$questionNumber번');
+                    };
+                  }
+                }
+                if (notSolveQuestion.isNotEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('안푼문제: ${notSolveQuestion}'),
+                      duration: Duration(seconds: 3),
+                    )
+                  );
+                }
+                log('$notSolveQuestion', name: 'notSolveQuestion');
+                getMbti();
+              },
+              child: Text('결과보기'),
+            ),
+            Text('$mbti'),
+          ],
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:mbti_app/home_page.dart';
+import 'package:mbti_app/question.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage()
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('엠비티아이'),
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          foregroundColor: Colors.black,
+          elevation: 0,
+        ),
+        body: HomePage(),
+      ),
     );
   }
 }
